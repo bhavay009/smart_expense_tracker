588567e022cae186052f06496cc19f3e58370dc7
bhavay goyal <bhavaygoyal1@gmail.com>
2026-04-26 00:56:48 +0530

    Added Add Expense screen UI and form

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
   int currentIndex = 0;
 
   final pages = [
-    const Center(child: Text("➕ Add Expense", style: TextStyle(fontSize: 24))),
+    const AddExpensePage(),
     const Center(
       child: Text("📋 Expense List", style: TextStyle(fontSize: 24)),
     ),
@@ -65,6 +65,73 @@ class _HomePageState extends State<HomePage> {
             icon: Icon(Icons.bar_chart),
             label: "Insights",
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddExpensePage extends StatefulWidget {
+  const AddExpensePage({super.key});
+
+  @override
+  State<AddExpensePage> createState() => _AddExpensePageState();
+}
+
+class _AddExpensePageState extends State<AddExpensePage> {
+  final amountController = TextEditingController();
+  final noteController = TextEditingController();
+
+  String selectedCategory = "Food";
+  DateTime selectedDate = DateTime.now();
+
+  final categories = ["Food", "Travel", "Shopping", "Bills", "Other"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              labelText: "Amount",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 15),
+
+          DropdownButtonFormField(
+            value: selectedCategory,
+            items: categories.map((item) {
+              return DropdownMenuItem(value: item, child: Text(item));
+            }).toList(),
+            onChanged: (value) {
+              setState(() {
+                selectedCategory = value!;
+              });
+            },
+            decoration: const InputDecoration(
+              labelText: "Category",
+              border: OutlineInputBorder(),
+            ),
+          ),
+
+          const SizedBox(height: 15),
+
+          TextField(
+            controller: noteController,
+            decoration: const InputDecoration(
+              labelText: "Note",
+              border: OutlineInputBorder(),
+            ),
+          ),
+
+          const SizedBox(height: 15),
+
+          ElevatedButton(onPressed: () {}, child: const Text("Save Expense")),
         ],
       ),
     );
